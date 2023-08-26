3d7b458616bcef7f33b4b7999aae46eea98f3daf
Konstantin <mezentsev.k@gmail.com>
2023-08-26 12:05:42 +0800

    Задача №1 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Массивы. Урок 2");
+    public static void main(String[] args) { //метод main
+        task1();
     }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        System.out.println("Задача №1");
+        int totalPay = 0;
+        for (int i = 0; i < arr.length; i++) {
+            totalPay = totalPay + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + totalPay + " рублей");
+
+    }
+
 }
